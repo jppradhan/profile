5bf6668e747774923cc0fd97c91ba1efbccacc6b
jppradhan <jyotiprakash@inkoniq.com>
2016-03-12 13:15:38 +0530

    fixes

--- routes/underconstruction.js
+++ routes/underconstruction.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('underconstruction');
+  res.render('underconstruction');
 });
 
 module.exports = router;
